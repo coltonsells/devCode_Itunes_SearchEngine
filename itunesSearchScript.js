785d5f596bb63f05110b1a0fa0fc189c44b790cd
coltonsells <coltonsells3@yahoo.com>
2018-09-10 15:05:47 -0500

    Initial Upload

---
+++ itunesSearchScript.js
@@ -0,0 +1,39 @@
+let input = document.getElementById("searchBar");
+
+function getData(lookup){
+    $.ajax({
+    type:"GET",
+    url: 'https://itunes.apple.com/search?term=' + lookup + "&entity=song&limit=100",
+    dataType: 'jsonp',
+    success: function pullData  ( resp) {
+        let a = resp.results
+        let para = document.getElementById("songList");
+        $(para).empty();
+        for( let x in a){
+            para.innerHTML += "<img src="+a[x].artworkUrl30+"> " +  "Artist: " + a[x].artistName + "  Album: "+a[x].collectionCensoredName+ "<br>  Track: " + "<a href="+ a[x].previewUrl+" target=\"_blank\">" + a[x].trackName + "</a><br>";
+        
+       }
+    }
+    });
+}
+
+function search(){
+    let text = document.getElementById("searchBar").value;
+    getData(text);
+}
+
+
+function enterSearch(event){
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("searchBar").click();
+    }
+}
+
+document.getElementById("submitButton").addEventListener("click", search());
+input.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submitButton").click();
+    }
+});
